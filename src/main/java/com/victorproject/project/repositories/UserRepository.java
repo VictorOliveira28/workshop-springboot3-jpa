cbe808e2daa1ec6b68f5a750b040e5da96e99144
Victor Oliveira <victor.oliveira20@icloud.com>
2023-08-02 15:22:33 -0300

    JPA Repository, dependency injection, database seeding

---
+++ src/main/java/com/victorproject/project/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.victorproject.project.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.victorproject.project.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+	
+	
+
+}
