df758403dba6db2bd1d575440aafa9b0f9e8181a
Victor Oliveira <victor.oliveira20@icloud.com>
2023-08-03 17:00:30 -0300

    Subtotal e Total methods

--- src/main/java/com/victorproject/project/entities/Order.java
+++ src/main/java/com/victorproject/project/entities/Order.java
@@ -104,6 +104,16 @@ public class Order implements Serializable {
 	public Set<OrderItem> getItem(){
 		return items;
 	}	
+	
+	public Double getTotal() {
+		
+		double sum = 0;
+		for(OrderItem x : items){
+			sum += x.getSubTotal();
+		}
+		
+		return sum;			
+	}
 
 	@Override
 	public int hashCode() {
